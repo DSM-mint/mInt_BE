9bd9c2802e82b88fa09c8478bf207a2eb84eb3ed
junha <goodjunha@gmail.com>
2023-07-04 11:33:32 +0900

    Setting :: 프로젝트 시작

---
+++ src/main/java/com/example/mInt/MIntApplication.java
@@ -0,0 +1,13 @@
+package com.example.mInt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MIntApplication.class, args);
+	}
+
+}
